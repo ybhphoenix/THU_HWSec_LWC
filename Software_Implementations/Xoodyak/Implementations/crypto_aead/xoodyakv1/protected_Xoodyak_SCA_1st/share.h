5891ee5c4815c74172d966712ed9d10b083a444f
OrionEver <82315214+OrionEver@users.noreply.github.com>
2022-12-12 11:36:35 +0800

    Update share.h

--- Software_Implementations/Xoodyak/Implementations/crypto_aead/xoodyakv1/protected_Xoodyak_SCA_1st/share.h
+++ Software_Implementations/Xoodyak/Implementations/crypto_aead/xoodyakv1/protected_Xoodyak_SCA_1st/share.h
@@ -1,13 +1,3 @@
-/*
-Implementation by Shuohang Peng 
-
-Thanks for Ronny Van Keer's pioneering implementation of the Xoodyak C code,
-our Xoodyak software protection implementation was adapted from his/her reference code. 
-
-The reference link for the original C code is as follows:
-https://csrc.nist.gov/Projects/lightweight-cryptography/finalists
-*/
-
 #include <stdint.h>
 #include "api.h"
 

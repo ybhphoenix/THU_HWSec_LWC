36e70d097b45922502a7e541863f40dcda7254dc
OrionEver <82315214+OrionEver@users.noreply.github.com>
2022-12-12 11:33:39 +0800

    Update encrypt.c

--- Software_Implementations/Xoodyak/Implementations/crypto_aead/xoodyakv1/protected_Xoodyak_SCA_1st/encrypt.c
+++ Software_Implementations/Xoodyak/Implementations/crypto_aead/xoodyakv1/protected_Xoodyak_SCA_1st/encrypt.c
@@ -1,11 +1,11 @@
 /*
 Implementation by Shuohang Peng 
 
-Thanks for Ronny Van Keer's pioneering implementation of the Xoodyak C code,
+Thanks for Sean B. Palmer's pioneering implementation of the Xoodyak C code,
 our Xoodyak software protection implementation was adapted from his/her reference code. 
 
 The reference link for the original C code is as follows:
-https://csrc.nist.gov/Projects/lightweight-cryptography/finalists
+https://github.com/sbp/xoocycle
 */
 
 #include "api.h"
